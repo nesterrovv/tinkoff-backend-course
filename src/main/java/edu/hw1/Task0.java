3675c935573a81ef45c6297582a16ea2aad0e7f8
nesterrovv <nesterov.iv2018@gmail.com>
2023-10-03 22:41:20 +0300

    added solution of first task of homework1

---
+++ src/main/java/edu/hw1/Task0.java
@@ -0,0 +1,12 @@
+package edu.hw1;
+
+import java.util.logging.Logger;
+
+public class Task0 {
+
+    public static void main(String[] args) {
+        Logger logger = Logger.getLogger("My logger");
+        logger.info("Hello world!");
+    }
+
+}
