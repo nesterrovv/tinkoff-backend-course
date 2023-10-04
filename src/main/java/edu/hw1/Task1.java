66abba95d7ab36dd3da9c00a4f2d2813f4629932
nesterrovv <nesterov.iv2018@gmail.com>
2023-10-04 13:29:42 +0300

    codestyle fixes #3 for hw1 task 1

--- src/main/java/edu/hw1/Task1.java
+++ src/main/java/edu/hw1/Task1.java
@@ -5,39 +5,35 @@ import java.util.concurrent.*;
 
 public class Task1 {
 
-    private final static Logger logger = Logger.getLogger("LOGGER");
+//    public static void main(String[] args) {
+//
+//    }
+
+    private final static Logger LOGGER = Logger.getLogger("LOGGER");
 
     private final static Long secondsInMinute = 60L;
 
-    @SuppressWarnings("MagicNumber")
     public long minutesToSeconds(String input) {
         try {
-            if (input == null) {
-                logger.warning("Input string is null");
-                return -1L;
-            }
-            if (!input.isBlank()) {
-                long totalSecondsNumber = 0L;
-                String[] time = input.split(":", 2);
-                if (time.length < 2) {
-                    logger.warning("Input is not like a time pattern.");
-                    return -1L;
+            if (input != null) {
+                if (!input.isBlank()) {
+                    long totalSecondsNumber = 0L;
+                    String[] time = input.split(":", 2);
+                    if (time.length == 2) {
+                        long minutes = Long.parseLong(time[0]);
+                        int seconds = Integer.parseInt(time[1]);
+                        if (!(minutes < 0L || seconds < 0 || seconds >= secondsInMinute)) {
+                            totalSecondsNumber += minutes * secondsInMinute;
+                            totalSecondsNumber += seconds;
+                            return totalSecondsNumber;
+                        }
+                    }
                 }
-                long minutes = Long.parseLong(time[0]);
-                int seconds = Integer.parseInt(time[1]);
-                if (minutes < 0L || seconds < 0 || seconds >= secondsInMinute) {
-                    logger.warning("Number of minutes/seconds are incorrect.");
-                    return -1L;
-                }
-                totalSecondsNumber += minutes * secondsInMinute;
-                totalSecondsNumber += seconds;
-                return totalSecondsNumber;
-            } else {
-                logger.warning("Input is empty.");
-                return -1L;
             }
+            LOGGER.warning("Input is incorrect.");
+            return -1L;
         } catch (NumberFormatException numberFormatException) {
-            logger.warning(numberFormatException.getMessage());
+            LOGGER.warning(numberFormatException.getMessage());
             return -1L;
         }
     }
