442ac71deec4f8c928b0f34d00f6bc49773f00c6
nesterrovv <nesterov.iv2018@gmail.com>
2023-10-07 22:52:33 +0300

    added solution of hw #1 task #6 and unit-tests for it

---
+++ src/main/java/edu/hw1/Task6.java
@@ -0,0 +1,65 @@
+package edu.hw1;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.logging.Logger;
+
+public class Task6 {
+
+    private final static Logger LOGGER = Logger.getLogger("LOGGER");
+    private static final int KAPREKARS_CONSTANT = 6174;
+    private static final int BASE = 10;
+    private static final int MINIMAL_THREE_DIGIT_NUMBER = 1000;
+    private static final int MAXIMAL_THREE_DIGIT_NUMBER = 9999;
+
+    public int countK(int n) {
+        try {
+            if (n < MINIMAL_THREE_DIGIT_NUMBER || n > MAXIMAL_THREE_DIGIT_NUMBER) {
+                throw new IncorrectArgumentException();
+            }
+            int number = n;
+            int counter = 0;
+            while (number != KAPREKARS_CONSTANT) {
+                number = modify(number);
+                counter++;
+            }
+            return counter;
+        } catch (IncorrectArgumentException incorrectArgumentException) {
+            LOGGER.warning(incorrectArgumentException.getMessage());
+            return -1;
+        }
+    }
+
+    private int modify(int n) {
+        int argument = n;
+        while (!(argument >= MINIMAL_THREE_DIGIT_NUMBER && argument <= MAXIMAL_THREE_DIGIT_NUMBER)) {
+            argument *= BASE;
+        }
+        String numberStr = Long.toString(argument);
+        char[] firstNumberDigits = numberStr.toCharArray();
+        Arrays.sort(firstNumberDigits);
+        String sortedNumberStr = new String(firstNumberDigits);
+        int first = Integer.parseInt(sortedNumberStr);
+        Character[] secondNumberDigits = new Character[numberStr.length()];
+        for (int i = 0; i < numberStr.length(); i++) {
+            secondNumberDigits[i] = numberStr.charAt(i);
+        }
+        Arrays.sort(secondNumberDigits, Collections.reverseOrder());
+        StringBuilder sortedNumberStr2 = new StringBuilder();
+        for (char digit : secondNumberDigits) {
+            sortedNumberStr2.append(digit);
+        }
+        int second = Integer.parseInt(sortedNumberStr2.toString());
+        return second - first;
+    }
+
+    static class IncorrectArgumentException extends Exception {
+
+        @Override
+        public String getMessage() {
+            return "Incorrect argument.";
+        }
+
+    }
+
+}
---
+++ src/test/java/edu/hw1/Task6Test.java
@@ -0,0 +1,36 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Task6Test {
+
+    private final Task6 task6 = new Task6();
+
+    @Test
+    @DisplayName("Base test")
+    public void testInputWithoutModifying() {
+        Assertions.assertEquals(5, task6.countK(6621));
+        Assertions.assertEquals(4, task6.countK(6554));
+        Assertions.assertEquals(3, task6.countK(1234));
+        Assertions.assertEquals(0, task6.countK(6174));
+        Assertions.assertEquals(3, task6.countK(3524));
+    }
+
+    @Test
+    @DisplayName("Zero saving necessity input test")
+    public void testInputWithZeroSavingNecessity() {
+        Assertions.assertEquals(5, task6.countK(1000));
+        Assertions.assertEquals(5, task6.countK(9998));
+    }
+
+    @Test
+    @DisplayName("Incorrect input test")
+    public void testIncorrectInput() {
+        Assertions.assertEquals(-1, task6.countK(0));
+        Assertions.assertEquals(-1, task6.countK(10000));
+        Assertions.assertEquals(-1, task6.countK(100));
+    }
+
+}
