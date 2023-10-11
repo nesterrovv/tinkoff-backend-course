31d20548e81f1c86cf25873e671faa1686e8bf50
nesterrovv <nesterov.iv2018@gmail.com>
2023-10-11 15:11:07 +0300

    modified tests for task #2, now it tests static method

--- src/test/java/edu/hw1/Task2Test.java
+++ src/test/java/edu/hw1/Task2Test.java
@@ -6,28 +6,26 @@ import org.junit.jupiter.api.Test;
 
 public class Task2Test {
 
-    private final Task2 task2 = new Task2();
-
     @Test
     @DisplayName("Base inputs test")
     public void testBaseInput() {
-        Assertions.assertEquals(4, task2.countNumberOfDigits(1234L));
-        Assertions.assertEquals(4, task2.countNumberOfDigits(1111L));
-        Assertions.assertEquals(4, task2.countNumberOfDigits(9000L));
+        Assertions.assertEquals(4, Task2.countNumberOfDigits(1234L));
+        Assertions.assertEquals(4, Task2.countNumberOfDigits(1111L));
+        Assertions.assertEquals(4, Task2.countNumberOfDigits(9000L));
     }
 
     @Test
     @DisplayName("Zeroes input test")
     public void testZeroInput() {
-        Assertions.assertEquals(1, task2.countNumberOfDigits(0L));
-        Assertions.assertEquals(1, task2.countNumberOfDigits(0000L));
+        Assertions.assertEquals(1, Task2.countNumberOfDigits(0L));
+        Assertions.assertEquals(1, Task2.countNumberOfDigits(0000L));
     }
 
     @Test
     @DisplayName("Big number input test")
     public void testBigNumberInput() {
-        Assertions.assertEquals(10, task2.countNumberOfDigits(9_999_999_999L));
-        Assertions.assertEquals(10, task2.countNumberOfDigits(9_999_999_990L));
+        Assertions.assertEquals(10, Task2.countNumberOfDigits(9_999_999_999L));
+        Assertions.assertEquals(10, Task2.countNumberOfDigits(9_999_999_990L));
     }
 
 }
