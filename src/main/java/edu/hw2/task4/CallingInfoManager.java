00b4f857998c9a96c7e0d19ba0972234d116e54a
nesterrovv <nesterov.iv2018@gmail.com>
2023-11-06 18:52:01 +0300

    fix of task #4 from hw #2 after the code review

--- src/main/java/edu/hw2/task4/CallingInfoManager.java
+++ src/main/java/edu/hw2/task4/CallingInfoManager.java
@@ -1,16 +1,24 @@
 package edu.hw2.task4;
 
-import java.util.logging.Logger;
-
 public final class CallingInfoManager {
-
-    private static final Logger LOGGER = Logger.getLogger("LOGGER");
 
     private CallingInfoManager() {}
 
-    public static CallingInfo callingInfo() {
-        StackTraceElement info = new Throwable().getStackTrace()[1];
-        return new CallingInfo(info.getClassName(), info.getMethodName());
+    public static CallingInfo callingInfo(Exception exception) {
+        if (exception == null) {
+            return null;
+        }
+        StackTraceElement[] stackTrace = exception.getStackTrace();
+        if (stackTrace.length > 1) {
+            StackTraceElement info = stackTrace[1];
+            return new CallingInfo(info.getClassName(), info.getMethodName());
+        }
+        return null;
     }
 
 }
+
+
+
+
+
