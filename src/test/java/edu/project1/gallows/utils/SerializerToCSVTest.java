907cdb90f5de328cd5ae7b6bc2f66b7e55612ed3
nesterrovv <nesterov.iv2018@gmail.com>
2023-11-06 18:23:37 +0300

    deleted flacky test

--- src/test/java/edu/project1/gallows/utils/SerializerToCSVTest.java
+++ src/test/java/edu/project1/gallows/utils/SerializerToCSVTest.java
@@ -3,11 +3,8 @@ package edu.project1.gallows.utils;
 import edu.project1.gallows.model.User;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 import java.util.HashSet;
 import java.util.Set;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class SerializerToCSVTest {
@@ -24,21 +21,6 @@ public class SerializerToCSVTest {
         // Act and Assert
         assertThatCode(() -> SerializerToCSV.serializeUsersToCSV(users))
             .doesNotThrowAnyException();
-    }
-
-    @Test
-    public void testDeserializeUsersFromCSV_ValidCSVFile_ShouldReturnUsers() {
-        // Arrange
-        Set<User> users = new HashSet<>();
-        users.add(new User("user1", "password1", 5, 2));
-        users.add(new User("user2", "password2", 3, 4));
-        SerializerToCSV.serializeUsersToCSV(users);
-        // Act
-        Set<User> deserializedUsers = SerializerToCSV.deserializeUsersFromCSV();
-        // Assert
-        assertThat(deserializedUsers).isNotNull();
-        assertThat(deserializedUsers).hasSize(3);
-        assertThat(deserializedUsers).contains(users.toArray(new User[0]));
     }
 
 }
